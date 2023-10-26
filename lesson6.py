bb082fe09a9125120956c0382f4a9e6eac9e1824
vlad1 <vlad1001v@gmail.com>
2023-10-26 10:05:47 +0300

    Урок 6

---
+++ lesson6.py
@@ -0,0 +1,34 @@
+#Завдання 1
+#my_list = [34, 105, 98, 123, 50, 67, 190, 89]
+
+#for value in my_list:
+#    if value > 100:
+#        print(value)
+
+#Завдання 2
+
+#my_list = [34, 105, 98, 123, 50, 67, 190, 89]
+#my_results = []
+
+#for value in my_list:
+#    if value > 100:
+#       my_results.append(value)
+
+#print(my_results)
+
+
+
+#Завдання 3
+#my_list = [10, 20]
+
+#if len(my_list) < 2:
+#    my_list.append(0)
+#else:
+#    sum_of_last_two = my_list[-1] + my_list[-2]
+#    my_list.append(sum_of_last_two)
+
+#print(my_list)
+
+
+
+
